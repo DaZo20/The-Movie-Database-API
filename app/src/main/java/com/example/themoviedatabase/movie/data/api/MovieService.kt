8f2b63982f507b0d42e8d1aa056d14edc1ae1e60
David Muñoz <david.munyoz97@gmail.com>
2023-03-03 19:41:37 +0100

    feat: Added MovieService for the endpoints

--- app/src/main/java/com/example/themoviedatabase/movie/data/api/MovieService.kt
+++ app/src/main/java/com/example/themoviedatabase/movie/data/api/MovieService.kt
@@ -1,8 +1,26 @@
 package com.example.themoviedatabase.movie.data.api
 
+import com.example.themoviedatabase.BuildConfig
+import com.example.themoviedatabase.movie.data.model.MoviesDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 //
 // Created by DaZo20 on 03/03/2023.
 // Copyright (c) 2023 DZ. All rights reserved.
 //
+private const val API_KEY = BuildConfig.API_KEY
+
 interface MovieService {
+
+    @GET("movie/popular")
+    suspend fun getAllPopularMovies(@Query ("api_key") api_key: String = API_KEY,
+                                    @Query("page") page: Int = 1): Response<MoviesDto?>
+
+    @GET("search/movie")
+    suspend fun getSearchedMovies(@Query ("api_key") api_key: String = API_KEY,
+                                  @Query("query") query: String,
+                                  @Query("page") page: Int = 1): Response<MoviesDto>
+
 }
