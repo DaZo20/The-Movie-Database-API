74b3541eedf463eb17fff76dd6ce1f6ad8f02d5e
David Muñoz <david.munyoz97@gmail.com>
2023-03-04 16:48:20 +0100

    feat: Added resource files for the presentation app

--- app/src/main/java/com/example/themoviedatabase/MainActivity.kt
+++ app/src/main/java/com/example/themoviedatabase/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.themoviedatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.themoviedatabase.databinding.ActivityMainBinding
+import com.example.themoviedatabase.movie.data.api.MovieService
+import com.example.themoviedatabase.movie.presentation.view.MovieFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+    private val movieFragment: Fragment by lazy { MovieFragment.newInstance() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        onReplaceFragment(MovieFragment())
+
     }
+    private fun onReplaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_view, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+
 }
