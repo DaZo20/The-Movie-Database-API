51a221c7c6539602a21b6f488a8f16c83aceaa8d
David Muñoz <david.munyoz97@gmail.com>
2023-03-05 19:14:36 +0100

    feat: Added Spanish imput for the api calls

--- app/src/main/java/com/example/themoviedatabase/movie/data/api/MovieService.kt
+++ app/src/main/java/com/example/themoviedatabase/movie/data/api/MovieService.kt
@@ -11,17 +11,20 @@ import retrofit2.http.Query
 // Copyright (c) 2023 DZ. All rights reserved.
 //
 private const val API_KEY = BuildConfig.API_KEY
+private const val SPANISH = "es-ES"
 
 interface MovieService {
 
     @GET("movie/popular")
     suspend fun getAllPopularMovies(@Query ("api_key") api_key: String = API_KEY,
+                                    @Query("language") language: String = SPANISH,
                                     @Query("page") page: Int = 1): Response<MoviesDto?>
 
 
     @GET("search/movie")
     suspend fun getSearchedMovies(@Query ("api_key") api_key: String = API_KEY,
                                   @Query("query") query: String,
+                                  @Query("language") language: String = SPANISH,
                                   @Query("page") page: Int = 1): Response<MoviesDto>
 
 }
